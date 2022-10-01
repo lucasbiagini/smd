6f41cf9e815c894f13d3a5e6e0545a60710f2a3a
Lucas Biagini <biaginisilva@gmail.com>
2022-10-01 15:44:21 +0200

    added new validation type

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('string_or_integer', function ($attribute, $value, $parameters, $validator) {
+            return is_string($value) || is_int($value);
+        });
     }
 }
